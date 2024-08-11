1c7243afb3a935e1eccb21cf2c39f289136bf6bf
Aman <jaman207168@gmail.com>
2024-08-11 21:43:10 +0530

    fake dataset

---
+++ src/assets/fakeData.js
@@ -0,0 +1,25 @@
+export const fakeData = [
+  {
+    $id: 1,
+    body: JSON.stringify(
+      'Resources:\n- Book: "You Don\'t Know JS: Scope & Closures" by Kyle Simpson.\n\n- Online Course: "JavaScript Patterns" on Udemy.\n\n- Articles:\n"Understanding JavaScript Closures" on Medium.\n\n"Mastering JavaScript Modules" on Dev.to.'
+    ),
+    colors: JSON.stringify({
+      id: "color-purple",
+      colorHeader: "FED0FD",
+      colorBody: "FEE5FD",
+      colorText: "#18181A",
+    }),
+    position: JSON.stringify({
+      x: 505, y: 10
+    }),
+  },
+  {
+    $id: 2,
+
+  },
+  {
+    $id: 3,
+    
+  },
+]
