7bfde5defac48a6d187e636add5ea31b865d2402
Aman <jaman207168@gmail.com>
2024-08-12 14:20:01 +0530

    autogrow

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
+
+  const textArearef = useRef(null);
+
+  useEffect(() => { 
+    autoGrow(textArearef);
+  }, []);
+
+  const autoGrow = (textarea) => {
+    const { current } = textArearef;
+
+    current.style.height = "auto";
+    current.style.height = current.scrollHeight + "px";
+  }
 
   return<div 
     className="card"
@@ -24,6 +37,10 @@ const NoteCard = ({ note }) => {
       <textarea
         style={{ color: colors.colorText }}
         defaultValue = {body}
+        ref={textArearef}
+        onInput={() => {
+          autoGrow(textArearef)
+        }}
       ></textarea>
     </div>
   </div>
