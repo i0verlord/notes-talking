9f45258816f045bf70e31aa91253fe3badbbccde
Aman <jaman207168@gmail.com>
2024-08-12 10:07:59 +0530

    temp data

--- src/assets/fakeData.js
+++ src/assets/fakeData.js
@@ -3,16 +3,14 @@ export const fakeData = [
     $id: 1,
     body: JSON.stringify(
       'Resources:\n- Book: "You Don\'t Know JS: Scope & Closures" by Kyle Simpson.\n\n- Online Course: "JavaScript Patterns" on Udemy.\n\n- Articles:\n"Understanding JavaScript Closures" on Medium.\n\n"Mastering JavaScript Modules" on Dev.to.'
-    ),
+    ), // Area Stores Data
     colors: JSON.stringify({
       id: "color-purple",
       colorHeader: "FED0FD",
       colorBody: "FEE5FD",
       colorText: "#18181A",
-    }),
-    position: JSON.stringify({
-      x: 505, y: 10
-    }),
+    }),  // Color of the card
+    position: JSON.stringify({ x: 505, y: 10 }), // Postion of the card
   },
   {
     $id: 2,
